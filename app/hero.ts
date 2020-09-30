d23d2162d566e823acd96816340ca57e8882ac8a
Pedro Diaz Villalobos <pdiazvil@gmu.edu>
2020-09-29 20:08:57 -0400

    first commit

---
+++ app/hero.ts
@@ -0,0 +1,5 @@
+export interface Hero {
+    portfolioID: number;
+    style: string;
+    empID: number;
+  }
---
+++ app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { portfolioID: 1, style: 'Modern', empID: 101 },
+  { portfolioID: 2, style: 'Minimalism', empID: 102  },
+  { portfolioID: 3, style: 'Rustic', empID: 103  }
+];
